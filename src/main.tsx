13630f33f36e3700c366c58e2351860f7b2d6216
gauravThakur-sgl <gaurav.thakur@shipglobal.in>
2025-02-25 16:22:58 +0530

    Added pwa feature

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,24 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./App";
+import { InstallPWA } from "./components/InstallPWA";
 
 createRoot(document.getElementById("root")!).render(
  <StrictMode>
   <App />
+  <InstallPWA />
  </StrictMode>
 );
+
+if ("serviceWorker" in navigator) {
+ window.addEventListener("load", () => {
+  navigator.serviceWorker
+   .register("/serviceWorker.js")
+   .then((registration) => {
+    console.log("Service Worker registered with scope:", registration.scope);
+   })
+   .catch((error) => {
+    console.log("Service Worker registration failed:", error);
+   });
+ });
+}
