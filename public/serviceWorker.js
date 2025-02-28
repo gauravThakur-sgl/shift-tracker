d7b3e124229b9743beafd6ecd21ebf170c5514ed
gauravThakur-sgl <gaurav.thakur@shipglobal.in>
2025-02-28 11:17:30 +0530

    updated service worker activate

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -11,8 +11,9 @@ self.addEventListener('install', (event) => {
     caches.open(CACHE_NAME)
       .then((cache) => {
         console.log('Opened cache');
-        return cache.addAll(urlsToCache);
+        cache.addAll(urlsToCache);
       })
+      .then(() => self.skipWaiting())
   );
 });
 self.addEventListener('activate', (event) => {
@@ -31,9 +32,6 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request);
-      })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
